4cc9b37f89b1ac8fdcc4c0ab50e3975bc3fdefe1
WesamAbadi <wisamabady7@gmail.com>
2022-04-18 13:16:37 +0200

    started

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import Chat from "./components/Chat";
+import SignIn from "./components/SignIn";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <SignIn />
+      <Chat />
+    </>
+  );
 }
 
 export default App;
---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+
+const firebaseapp = firebase.initializeApp({
+  apiKey: "AIzaSyC0zhJhNNZQ7LX4OE2Xdh_ix5u4qVyVUWY",
+  authDomain: "chatbase-af219.firebaseapp.com",
+  projectId: "chatbase-af219",
+  storageBucket: "chatbase-af219.appspot.com",
+  messagingSenderId: "539230130249",
+  appId: "1:539230130249:web:61b7247fd6143bfefeef98",
+  measurementId: "G-B1207J3HGV",
+});
+
+const db = firebaseapp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
