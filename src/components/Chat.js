2181b7905534bc193f063c4d5f9b43626d56bdb2
WesamAbadi <wisamabady7@gmail.com>
2022-04-20 17:57:33 +0200

    Quota exceeded a7a

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,7 +1,13 @@
 import React, { useState, useEffect } from "react";
 import SignOut from "./SignOut";
 //import { db } from "../firebase";
-import { doc, getFirestore, onSnapshot, collection } from "firebase/firestore";
+import {
+  doc,
+  getDocs,
+  getFirestore,
+  onSnapshot,
+  collection,
+} from "firebase/firestore";
 //import { getDatabase, ref, query, orderByChild } from "firebase/database";
 import "firebase/compat/firestore";
 import SendMessage from "./SendMessage";
@@ -9,16 +15,24 @@ const db = getFirestore();
 function Chat() {
   const [messages, setMessages] = useState([]);
 
-  useEffect(() => {
-    const test = onSnapshot(doc(collection(db, "messages")), (doc) => {
-      console.log(doc.data());
-    });
-  }, []);
-  //console.log(db);
+  function put(text) {
+    setMessages(text);
+  }
+  useEffect(() => {}, []);
+  getDocs(collection(db, "messages")).then((querySnapshot) => {
+    const newUserDataArray = querySnapshot.docs.map((doc) => ({
+      ...doc.data(),
+      id: doc.id,
+    }));
+    //newUserDataArray.forEach((element) => put(element.text));
+  });
+  //console.log(messages);
   return (
     <div>
       <SignOut />
+
       {messages}
+
       <h5>chat js</h5>
       <SendMessage />
     </div>
